b25f37c153f41a6c9f042f924b2c75b53ce31c4b
唐建峰 <tangjianfeng@mi-ya.com.cn>
2018-10-26 15:23:13 +0800

    [points] split array into groups for better performance

--- src/points/index.js
+++ src/points/index.js
@@ -1,5 +1,6 @@
 const invariant = require('invariant');
 const { kdTree } = require('./kdTree')
+const MAXSIZE = 30000
 /**
  * draw mass points without blockƒ
  * */
@@ -10,12 +11,10 @@ export default class Points {
       Array.isArray(dataList),
       'DataList should be array'
     )
-    // invariant(
-    //   typeof drawer === "function",
-    //   'Parameter drawer should be function'
-    // )
-
-    this.cursor = 0
+    invariant(
+      typeof drawer === "function",
+      'Parameter drawer should be function'
+    )
 
     this.processedDataList = this.generateBinaryData(dataList)
     this.drawer = drawer
@@ -23,11 +22,6 @@ export default class Points {
 
   /** indicate the index of points data array */
   cursor = 0
-
-  /**
-   * saving waiting points to draw
-   */
-  stack = []
 
   /** start or stop render */
   pause = false
@@ -41,15 +35,28 @@ export default class Points {
     if(this.pause) return;
 
     const { processedDataList, cursor } = this
-    const endIndex = processedDataList.length
+    const endIndexOut = processedDataList.length
+    // if outer length is 0, empty
+    if(endIndexOut === 0) return
+    // last array length
+    const endIndexLast = processedDataList[endIndexOut - 1].length
+    // total count
+    const endIndex = (endIndexOut - 1) * MAXSIZE + endIndexLast
+
     if(cursor > endIndex) return
 
     const timeLeft = deadLine.timeRemaining()
     const count = Math.floor(timeLeft) * 256
-    // const pointArr = processedDataList.slice(cursor, count)
     let current = cursor
+
     while(current < endIndex && current < cursor + count) {
-      this.drawer && this.drawer(processedDataList[current])
+      const currentOut = Math.floor(current / MAXSIZE)
+      const tailIndex = current - currentOut * MAXSIZE
+      const point = processedDataList[currentOut][tailIndex]
+      this.drawer && this.drawer({
+        x: point[0],
+        y: point[1]
+      })
       current += 1
     }
     this.cursor = current
@@ -89,18 +96,40 @@ export default class Points {
     const kdData = new kdTree(dataList, function (a, b) {
       return Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2)
     }, ['x', 'y'])
-    const stack = [kdData.root]
+    let firstStack = [kdData.root]
+    let currentStack = firstStack
+    const stack = [firstStack]
+
+    // 2-d array
     let newDataListArray = []
-    while(stack.length) {
-      const node = stack.shift()
-      if (node.left) {
-        stack.push(node.left)
+    const listArraySet = [newDataListArray]
+
+    // convert tree to binary heap
+    while(stack.length && firstStack && firstStack.length) {
+      // if stack is empty, exit
+      const node = firstStack.shift()
+      if(firstStack.length === 0 && stack.length > 1) {
+        stack.shift()
+        firstStack = stack[0]
       }
-      newDataListArray.push(node.obj)
+      if(currentStack.length > MAXSIZE) {
+        currentStack = []
+        stack.push(currentStack)
+      }
+      if (node.left) {
+        currentStack.push(node.left)
+      }
+      if(newDataListArray.length >= MAXSIZE) {
+        newDataListArray = []
+        listArraySet.push(newDataListArray)
+      }
+      // save as array
+      const {x, y} = node.obj
+      newDataListArray.push([x, y])
       if(node.right) {
-        stack.push(node.right)
+        currentStack.push(node.right)
       }
     }
-    return newDataListArray
-  }d
+    return listArraySet
+  }
 }
