72fcce27c0be580dd6ae51fb79a9231a0a420d5d
唐大咸鱼 <daxianyu@outlook.com>
2019-01-03 18:02:49 +0800

    [Arcs] Add animated Arcs.

---
+++ src/Arcs/animated.js
@@ -0,0 +1,171 @@
+import { getMidperpandicular, getRadOfVector, getTransformedRadiusAndCenter } from './utils';
+import Scheduler from '../Scheduler';
+import TwoDArray from '../base/2dArray';
+
+export default class Arcs extends Scheduler {
+  /**
+   * @param {object} ctx - canvas context;
+   * @param {object} options: rate: L/R rate;
+   * */
+  constructor(ctx, options) {
+    const { data, coordinateTransformation, rate = 0.5, lazy = true } = options;
+    super({ data: new TwoDArray(data), lazy });
+    this.ctx = ctx;
+    this.coordinateTransformation = coordinateTransformation;
+    this.rate = rate;
+    this.generateRelativeCenterAndRadius(rate);
+  }
+
+  generateRelativeCenterAndRadius(rate) {
+    /** Generate initial radius and centers,
+     * any others could calculate by rotation or translation or scale
+     */
+    const radiusAndCenters = getMidperpandicular([0, 0], [1, 0])(rate);
+    /* With two points and one radius, we can obtain two circles */
+    const { centers, radius } = radiusAndCenters;
+    this.relativeCenterAndRadius = {
+      centers,
+      radius,
+    };
+  }
+
+  /**
+   * This is the only API to modify grid.
+   * New options will be merged with old options, such that one could update grid by calling
+   * this API with differentials.
+   */
+  setOptions = (options) => {
+    const {
+      coordinateTransformation = this.coordinateTransformation,
+      data,
+      rate = this.rate,
+    } = options;
+    if (data && data !== this.data) {
+      this.data = new TwoDArray(data);
+    }
+    this.coordinateTransformation = coordinateTransformation;
+    this.rate = rate;
+    this.generateRelativeCenterAndRadius(rate);
+  };
+
+  /* Implement Schedule dataHandler */
+  dataHandler(index, data) {
+    this.drawArcs(data, this.rate);
+  }
+
+  drawArcAsync(xc, yc, radius, startAngle, endAngle, clockwise) {
+    const tick = (endAngle - startAngle) / 20;
+    let tempEndAngle = startAngle;
+    const drawer = () => {
+      this.animateIndex = window.requestAnimationFrame(() => {
+        this.ctx.beginPath();
+        this.ctx.arc(
+          xc,
+          yc,
+          radius,
+          startAngle,
+          tempEndAngle,
+          clockwise,
+        );
+        this.ctx.stroke();
+        /**
+         * 如果靠得足够近
+         */
+        if (Math.abs(tempEndAngle - endAngle) < 0.5 * Math.abs(tick)) {
+          this.loopStack();
+        } else {
+          tempEndAngle += tick;
+          drawer();
+        }
+      });
+    };
+    drawer();
+  }
+
+  /**
+   * 如果沿着顺时针方向, 起点需要走大于半圆的路径才能抵达终点, 则为优弧,
+   * 如果终点角度小于起点角度, 终点角度加上2π后再计算
+   * 如果是优弧, 则转为逆时针(true), 否则为顺时针(false)
+   * @param startAngle
+   * @param endAngle
+   */
+  static getArcClockwise(startAngle, endAngle) {
+    if (endAngle < startAngle) {
+      endAngle += (2 * Math.PI);
+    }
+    return (endAngle - startAngle > Math.PI);
+  }
+
+  /**
+   *
+   * @param startAngle
+   * @param endAngle
+   */
+  static getDeliveredEndAngle(startAngle, endAngle) {
+    if (endAngle < startAngle) {
+      endAngle += (2 * Math.PI);
+      if (endAngle - startAngle > Math.PI) {
+        return endAngle - 2 * Math.PI;
+      }
+    }
+    if (startAngle < endAngle) {
+      if (endAngle - startAngle > Math.PI) {
+        return endAngle - 2 * Math.PI;
+      }
+    }
+    return endAngle;
+  }
+
+  drawArc(center, points, radius) {
+    const [xc, yc] = center;
+    const [point1, point2] = points;
+    const [xp1, yp1] = point1;
+    const [xp2, yp2] = point2;
+    const startAngle = getRadOfVector([xp1 - xc, yp1 - yc]);
+    const endAngle = getRadOfVector([xp2 - xc, yp2 - yc]);
+    this.drawArcAsync(
+      xc,
+      yc,
+      radius,
+      startAngle,
+      Arcs.getDeliveredEndAngle(startAngle, endAngle),
+      Arcs.getArcClockwise(startAngle, endAngle),
+    );
+  }
+
+  drawArcs(points) {
+    let [point1, point2] = points;
+    /**
+     * Transform other unit to x-y;
+     * */
+    point1 = this.coordinateTransformation(point1);
+    point2 = this.coordinateTransformation(point2);
+    points = [point1, point2];
+
+    const { centers, radius } = this.relativeCenterAndRadius;
+    /* 只是作为一种随机, 但是保证该条线始终固定 */
+    if (point1[0] - point2[0] > point1[1] - point2[1]) {
+      const {
+        center,
+        radius: transformedRadius,
+      } = getTransformedRadiusAndCenter(points, centers[0], radius);
+      this.drawArc(center, points, transformedRadius);
+    } else {
+      const {
+        center,
+        radius: transformedRadius,
+      } = getTransformedRadiusAndCenter(points, centers[1], radius);
+      this.drawArc(center, points, transformedRadius);
+    }
+  }
+
+  shouldScheduleStop(current, point) {
+    return true;
+  }
+
+  /* Uniform method */
+  render() {
+    window.cancelAnimationFrame(this.animateIndex);
+    this.start();
+  }
+}
